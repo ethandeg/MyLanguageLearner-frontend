8ce7afc3823cfe990ff96030cdc9bed87cefcae6
Ethan Degenhardt <ethandeg1996@gmail.com>
2021-05-06 21:06:44 -0500

    styled unit page a bit

--- src/unitComponents/Learn.js
+++ src/unitComponents/Learn.js
@@ -1,33 +1,33 @@
-import {useDispatch, useSelector} from "react-redux"
-import {useEffect} from "react"
-import {getUnits} from "../actions/actions"
-import {useParams} from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
+import { getUnits } from "../actions/actions"
+import { useParams } from "react-router-dom"
 import Unit from "./Unit"
-import {loadCompletedLessons} from "../actions/actions"
+import { loadCompletedLessons } from "../actions/actions"
 const Learn = () => {
-    const {languageCode} = useParams()
+    const { languageCode } = useParams()
     const units = useSelector(state => state.units)
     const user = useSelector(state => state.userInfo)
     const userLanguage = useSelector(state => state.userLanguages.find(lang => lang.languageCode === languageCode))
     const dispatch = useDispatch()
 
     useEffect(() => {
-        if(!units && user.username){
+        if (!units && user.username) {
             dispatch(getUnits())
         }
-        if(user.username && !userLanguage.completedLessons){
+        if (user.username && !userLanguage.completedLessons) {
             dispatch(loadCompletedLessons(user.username, languageCode))
         }
 
-  
+
     }, [dispatch, user, languageCode])
     return (
-        <>
-        <h1 className="title is-2 has-text-primary">{languageCode}</h1>
-        {units && units.map(unit => (
-            <Unit unit={unit} key={unit.id}/>
-        ))}
-        </>
+        <div className="container">
+            <h1 className="title is-2 has-text-primary">{languageCode}</h1>
+            {units && units.map(unit => (
+                <Unit unit={unit} key={unit.id} />
+            ))}
+        </div>
     )
 }
 
--- src/unitComponents/Unit.js
+++ src/unitComponents/Unit.js
@@ -1,12 +1,22 @@
 
-const Unit = ({unit}) => {
+const Unit = ({ unit }) => {
     return (
-        <ul>
-            <li>{unit.unitName}</li>
-            <li>{unit.unitNumber}</li>
-            <li>{unit.id}</li>
-            <li>{unit.subUnits}</li>
-        </ul>
+        <article className="panel is-primary">
+            <p className="panel-heading">{unit.unitName} - {unit.unitNumber}</p>
+            <div className="panel-block"></div>
+
+            {unit.subUnits.map((sub, i) => (
+                <>
+                    <a className="panel-block is-active">
+                        <span className="icon is-left">
+                            <i className="fas fa-book" aria-hidden="true"></i>
+                        </span>
+                        Lesson {i} - Sub-Unit {sub}
+                    </a>
+                </>
+            ))}
+
+        </article>
     )
 }
 
