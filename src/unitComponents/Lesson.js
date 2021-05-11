3c984b7691c1cfaca02212fb79bbf6ebd3de77ca
Ethan Degenhardt <ethandeg1996@gmail.com>
2021-05-11 13:26:04 -0500

    refactored for new backend response

--- src/unitComponents/Lesson.js
+++ src/unitComponents/Lesson.js
@@ -1,19 +1,12 @@
 const Lesson = ({subUnit}) => {
     console.log(subUnit)
+    //have first lesson be simply going through all of the flash cards
     return (
         <ul>
-            {subUnit && subUnit.material.map((mat, i) => (
-                mat.map((seg,j) => (
-                    <div key={`${i}-${j}`}>
-                        <li>{seg.segment}</li>
-                        <li>{seg.translation}</li>
-                    </div>
-                ))
-                // <div key={i}>
-                // <li>{mat[i].segment}</li>
-                // <li>{mat[i].translation}</li>
-                // </div>
-            ))}
+
+        {subUnit && subUnit.material.map((mat, i) => (
+            <li key={i}>{mat.segment} - {mat.translation.join(', ')}</li>
+        ))}
         </ul>
     )
 }
